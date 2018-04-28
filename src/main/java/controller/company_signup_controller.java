09b376c03263c37bf4e52b69b66e3c918aab099d
angona1996 <mimtaz96@gmail.com>
2018-04-28 13:49:58 +0600

    Company sign up is done

--- src/main/java/controller/company_signup_controller.java
+++ src/main/java/controller/company_signup_controller.java
@@ -1,47 +1,56 @@
+
 package controller;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+
+import data.model.company_signup_model;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import data.model.company_signup_model;
-@ManagedBean(name="test")
-@SessionScoped
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
+@ManagedBean(
+        name = "test"
+)
+@SessionScoped
 public class company_signup_controller {
+    company_signup_model obj = new company_signup_model();
+
     public company_signup_controller() {
     }
 
-   public ArrayList<company_signup_model>getCheck() {
-        ArrayList<company_signup_model> comm = new ArrayList<company_signup_model>();
-        try{
+    public company_signup_model getObj() {
+        return this.obj;
+    }
+
+    public void setObj(company_signup_model obj) {
+        this.obj = obj;
+    }
+
+    public String signup() {
+        try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection con= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcs","projectpaths","Oracle_1");
-            Statement st=con.createStatement();
-            ResultSet rs=st.executeQuery("select* from test");
-            while(rs.next())
-            {
-                company_signup_model com=new company_signup_model();
-                com.setId(rs.getInt(1));
-                com.setName(rs.getString(2));
-                comm.add(com);
-
-            }
-
-
-
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "projectpaths", "Oracle_1");
+            Statement st = con.createStatement();
+            String b = obj.getComapny_name();
+            String c = obj.getOwner_name();
+            String add = obj.getAddress();
+            int contact = obj.getContact_no();
+            String email = obj.getEmail();
+            String representitive = obj.getRepresentitive_name();
+            String username = obj.getUsername();
+            String pass = this.obj.getPassword();
+            boolean salary = obj.isSalary_assurance();
+            boolean ins = obj.isInsurance();
+            boolean fire = obj.isFire_safety();
+            String a = "insert into company_signup(name,owner_name,address,contact_no,email,representitive_name,username,password,salary_assuarnce,insuarnce,fire_safety) values('" + b + "','" + c + "','" + add + "','" + contact + "','" + email + "','" + representitive + "','" + username + "','" + pass + "','" + salary + "','" + ins + "','" + fire + "')";
+            st.executeUpdate(a);
+            return "success";
+        } catch (Exception var15) {
+            return "false";
         }
-        catch (Exception e){
-            System.out.println(e);
-        }
-        return comm;
-
     }
-   public String to_next(){
+
+    public String to_next() {
         return "success";
-
     }
-
 }
--- src/main/java/data/model/company_signup_model.java
+++ src/main/java/data/model/company_signup_model.java
@@ -1,26 +1,108 @@
 package data.model;
 
 public class company_signup_model {
-    private int id;
-    private String name;
+   private String comapny_name;
+   private String owner_name;
+   private String address;
+   private  int contact_no;
+   private  String email;
+   private String representitive_name;
+   private  String username;
+   private String password;
+   private boolean salary_assurance;
+   private boolean insurance;
+   private boolean fire_safety;
+
+    public String getComapny_name() {
+        return comapny_name;
+    }
+
+    public void setComapny_name(String comapny_name) {
+        this.comapny_name = comapny_name;
+    }
+
+    public String getOwner_name() {
+        return owner_name;
+    }
+
+    public void setOwner_name(String owner_name) {
+        this.owner_name = owner_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getContact_no() {
+        return contact_no;
+    }
+
+    public void setContact_no(int contact_no) {
+        this.contact_no = contact_no;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRepresentitive_name() {
+        return representitive_name;
+    }
+
+    public void setRepresentitive_name(String representitive_name) {
+        this.representitive_name = representitive_name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isSalary_assurance() {
+        return salary_assurance;
+    }
+
+    public void setSalary_assurance(boolean salary_assurance) {
+        this.salary_assurance = salary_assurance;
+    }
+
+    public boolean isInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(boolean insurance) {
+        this.insurance = insurance;
+    }
+
+    public boolean isFire_safety() {
+        return fire_safety;
+    }
+
+    public void setFire_safety(boolean fire_safety) {
+        this.fire_safety = fire_safety;
+    }
 
     public company_signup_model() {
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
